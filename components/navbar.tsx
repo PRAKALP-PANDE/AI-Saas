9e83a4fa6d13e648c0c1cc96ec762ce8f7ce7f41
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-09-21 02:06:17 +0530

    Dashboard Completed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 const Navbar = () => {
     return (
         <div className="flex item-center p-4">
-            <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu />
-            </Button>
+            <MobileSidebar/>
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/" />
             </div>
